7916d7daff0cbdf33cd9b4619c39e76bc6de92df
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2026-04-30 20:45:57 -0500

    Comentarios con los comandos utilizables

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask
 from .db import init_app as init_db_app
 
+# flask --app flaskr run --debug
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
